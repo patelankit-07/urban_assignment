facfc94de6accef4ee63f3eb1379f67c71c8502f
Ankit Patel <ankitpatel.edugaon@gmail.com>
2024-08-13 17:19:44 +0530

    project done

--- lib/utils/screens/streaks_screen.dart
+++ lib/utils/screens/streaks_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
@@ -81,7 +82,6 @@ class _StreaksScreenState extends State<StreaksScreen> {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
@@ -102,7 +102,7 @@ class _StreaksScreenState extends State<StreaksScreen> {
                       children: [
                         Expanded(
                           child: Center(
-                            child: const Text(
+                            child: Text(
                               'Streaks',
                               style: TextStyle(
                                   fontSize: 25, fontWeight: FontWeight.bold),
@@ -119,12 +119,21 @@ class _StreaksScreenState extends State<StreaksScreen> {
                       TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
                     ),
                     SizedBox(height: screenHeight * 0.02),
-                    SizedBox(
-                      width: screenWidth * 0.9,
-                      child: _streakDaysCard(screenHeight, screenWidth),
+                    StreamBuilder<int>(
+                      stream: getTrueCountStream(),
+                      builder: (context, snapshot) {
+                        final count = snapshot.data ?? 0;
+                        return _streakDaysCard(screenHeight, screenWidth, count);
+                      },
                     ),
                     SizedBox(height: screenHeight * 0.02),
-                    _dailySticks(screenHeight),
+                    StreamBuilder<int>(
+                      stream: getTrueCountStream(),
+                      builder: (context, snapshot) {
+                        final count = snapshot.data ?? 0;
+                        return _dailySticks(screenHeight, count);
+                      },
+                    ),
                     SizedBox(height: screenHeight * 0.02),
                     _graphChart(screenWidth, screenHeight),
                     SizedBox(height: screenHeight * 0.02),
@@ -150,33 +159,36 @@ class _StreaksScreenState extends State<StreaksScreen> {
     );
   }
 
-  Widget _streakDaysCard(double screenHeight, double screenWidth) {
-    return Card(
-      margin: EdgeInsets.zero,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(12),
-      ),
-      color: const Color.fromRGBO(242, 232, 235, 1),
-      child: Padding(
-        padding: EdgeInsets.all(screenHeight * 0.025),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Streak Days',
-              style: TextStyle(fontSize: 16, color: Colors.grey[700]),
-            ),
-            const Text(
-              '2',
-              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-            ),
-          ],
+  Widget _streakDaysCard(double screenHeight, double screenWidth, int count) {
+    return SizedBox(
+      width: screenWidth * 0.9, // Adjust the width percentage as needed
+      child: Card(
+        margin: EdgeInsets.zero,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        color: const Color.fromRGBO(242, 232, 235, 1),
+        child: Padding(
+          padding: EdgeInsets.all(screenHeight * 0.025),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Streak Days',
+                style: TextStyle(fontSize: 16, color: Colors.grey[700]),
+              ),
+              Text(
+                count.toString(),
+                style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 
-  Widget _dailySticks(double screenHeight) {
+  Widget _dailySticks(double screenHeight, int count) {
     return Container(
       padding: EdgeInsets.all(screenHeight * 0.025),
       child: Column(
@@ -186,8 +198,8 @@ class _StreaksScreenState extends State<StreaksScreen> {
             'Daily Streak',
             style: TextStyle(fontSize: 16, color: Colors.grey[700]),
           ),
-          const Text(
-            '2',
+          Text(
+            count.toString(),
             style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
           ),
           SizedBox(height: screenHeight * 0.01),
@@ -214,7 +226,8 @@ class _StreaksScreenState extends State<StreaksScreen> {
 
   Widget _graphChart(double screenWidth, double screenHeight) {
     return Padding(
-      padding: EdgeInsets.all(screenHeight * 0.01),
+      padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.02),
+      // Adjust horizontal padding
       child: Column(
         children: [
           SizedBox(
@@ -225,24 +238,35 @@ class _StreaksScreenState extends State<StreaksScreen> {
               borderData: FlBorderData(show: false),
               lineBarsData: [
                 LineChartBarData(
-                  spots: _chartData[_selectedInterval]!,
+                  spots: _chartData[_selectedInterval] ?? [],
                   isCurved: true,
-                  color: const Color.fromRGBO(150, 79, 102, 1),
-                  barWidth: 2,
                   dotData: const FlDotData(show: false),
+                  belowBarData: BarAreaData(
+                    show: true,
+                    gradient: LinearGradient(
+                      colors: [
+                        const Color(0xFF7C4DFF).withOpacity(0.5),
+                        const Color(0xFF7C4DFF).withOpacity(0.0),
+                      ],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                    ),
+                  ),
+                  color: const Color(0xFF7C4DFF),
+                  barWidth: 3,
                 ),
               ],
             )),
           ),
-          SizedBox(height: screenHeight * 0.05),
+          SizedBox(height: screenHeight * 0.01),
           Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              _buildChartIntervalButton("1D", screenWidth),
-              _buildChartIntervalButton("1W", screenWidth),
-              _buildChartIntervalButton("1M", screenWidth),
-              _buildChartIntervalButton("3M", screenWidth),
-              _buildChartIntervalButton("1Y", screenWidth),
+              _intervalButton('1D', screenWidth),
+              _intervalButton('1W', screenWidth),
+              _intervalButton('1M', screenWidth),
+              _intervalButton('3M', screenWidth),
+              _intervalButton('1Y', screenWidth),
             ],
           ),
         ],
@@ -250,29 +274,29 @@ class _StreaksScreenState extends State<StreaksScreen> {
     );
   }
 
-  Widget _buildChartIntervalButton(String label, double screenWidth) {
-    return GestureDetector(
-      onTap: () {
-        setState(() {
-          _selectedInterval = label;
-        });
-      },
-      child: Container(
-        decoration: BoxDecoration(
-            borderRadius: const BorderRadius.all(Radius.circular(10)),
-            color: _selectedInterval == label
-                ? const Color.fromRGBO(150, 79, 102, 1)
-                : Colors.amber),
-        padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.02),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text(
-            label,
-            style: TextStyle(
+  Widget _intervalButton(String label, double screenWidth) {
+    return Flexible(
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            _selectedInterval = label;
+          });
+        },
+        child: Container(
+          decoration: BoxDecoration(
+              borderRadius: const BorderRadius.all(Radius.circular(10)),
               color: _selectedInterval == label
-                  ? Colors.white
-                  : Colors.black,
-              fontSize: 14,
+                  ? const Color.fromRGBO(150, 79, 102, 1)
+                  : Colors.amber),
+          padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.02),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              label,
+              style: TextStyle(
+                color: _selectedInterval == label ? Colors.white : Colors.black,
+                fontSize: 14,
+              ),
             ),
           ),
         ),
@@ -282,15 +306,23 @@ class _StreaksScreenState extends State<StreaksScreen> {
 
   Widget _logoutButton(double screenWidth) {
     return IconButton(
-      icon: Icon(
-        Icons.logout,
-        color: Colors.red[700],
-        size: screenWidth * 0.08,
-      ),
+      icon: const Icon(Icons.logout),
+      color: const Color.fromRGBO(124, 77, 255, 1),
       onPressed: () async {
         await FirebaseAuth.instance.signOut();
-        Get.offAll(const LoginScreen());
+        Get.to(() => const LoginScreen());
       },
     );
+  }
+
+  Stream<int> getTrueCountStream() {
+    return FirebaseFirestore.instance
+        .collection('skincare_routines')
+        .where('isChecked', isEqualTo: true)
+        .snapshots()
+        .map((snapshot) {
+      print('Document count: ${snapshot.docs.length}'); // Debug line
+      return snapshot.docs.length;
+    });
   }
 }
